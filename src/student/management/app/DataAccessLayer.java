e92fef19cc06b37b3a10e02df191a001e4dc9e44
Matt Maree <matt.maree@tracker.co.za>
2018-08-28 10:58:05 +0200

    Added Course & Subject Update

--- src/student/management/app/DataAccessLayer.java
+++ src/student/management/app/DataAccessLayer.java
@@ -16,136 +16,172 @@ import java.util.List;
  * @author matt.maree
  */
 public class DataAccessLayer {
-    public Connection Connect(){
+
+    public Connection Connect() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            return DriverManager.getConnection("jdbc:oracle:thin:hr/hr@localhost:1521:orcl","marline","oracle");
+            return DriverManager.getConnection("jdbc:oracle:thin:hr/hr@localhost:1521:orcl", "marline", "oracle");
         } catch (Exception ex) {
             Logger.getLogger(DataAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
-    } 
-    public List<Student> GetStudent() throws SQLException{
+    }
+
+    public List<Student> GetStudent() throws SQLException {
         List<Student> studentList = new ArrayList<>();
         Statement st = Connect().createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM Students");
-        while(rs.next()){
+        while (rs.next()) {
             studentList.add(new Student(rs.getInt(0),
-                                        rs.getString(1), 
-                                        rs.getString(2), 
-                                        rs.getString(3),
-                                        rs.getString(4),
-                                        rs.getString(5), 
-                                        rs.getString(6)));
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6)));
         }
         return studentList;
     }
-public List<Lecturer> GetLecturers() throws SQLException{
+
+    public List<Lecturer> GetLecturers() throws SQLException {
         List<Lecturer> lecturerList = new ArrayList<>();
         Statement st = Connect().createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM Lecturers");
-        while(rs.next()){
+        while (rs.next()) {
             lecturerList.add(new Lecturer(rs.getInt(0),
-                                        rs.getString(1), 
-                                        rs.getString(2), 
-                                        rs.getString(3),
-                                        rs.getString(4),
-                                        rs.getString(5), 
-                                        rs.getString(6)));
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6)));
         }
         return lecturerList;
     }
-    public List<Subject> GetSubjects() throws SQLException{
+
+    public List<Subject> GetSubjects() throws SQLException {
         List<Subject> subjectList = new ArrayList<>();
         Statement st = Connect().createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM Subjects");
-        while(rs.next()){
+        while (rs.next()) {
             subjectList.add(new Subject(rs.getInt(0),
-                                        rs.getString(1), 
-                                        rs.getInt(2),
-                                        rs.getInt(3)));
+                    rs.getString(1),
+                    rs.getInt(2),
+                    rs.getInt(3)));
         }
         return subjectList;
     }
-    
-     public Student insertStudent(Student student) throws SQLException{
-         String sql = "INSERT INTO Students (Student_Name, Student_Surname, Student_Age, Student_Gender, Student_Phone, Student_Email) VALUES (?, ?, ?, ?, ?, ?)";
-         String lastRow = "SELECT TOP 1 FROM Students ORDER BY DESC";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, student.getName());
-         ps.setString(1, student.getSurname());
-         ps.setString(2, student.getBirthDate());
-         ps.setString(3, student.getGender());
-         ps.setString(4, student.getPhone());
-         ps.setString(5, student.getEmail());
-         ps.executeUpdate();
-         ResultSet rs = ps.executeQuery(lastRow);
-         return new Student(rs.getInt(0),rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6));
-         
-     }
-     
-     public Lecturer insertLecturer(Lecturer lecturer) throws SQLException{
-         String sql = "INSERT INTO Lecturers (Lecturer_Name, Lecturer_Surname, Lecturer_Age, Lecturer_Gender, Lecturer_Phone, Lecturer_Email) VALUES (?, ?, ?, ?, ?, ?)";
-         String lastRow = "SELECT TOP 1 FROM Lecturers ORDER BY DESC";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, lecturer.getName());
-         ps.setString(1, lecturer.getSurname());
-         ps.setString(2, lecturer.getBirthDate());
-         ps.setString(3, lecturer.getGender());
-         ps.setString(4, lecturer.getPhone());
-         ps.setString(5, lecturer.getEmail());
-         ps.executeUpdate();
-         ResultSet rs = ps.executeQuery(lastRow);
-         return new Lecturer(rs.getInt(0),rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6));
-         
-     }
-     
-     public Subject insertSubject(Subject subject) throws SQLException{
-         String sql = "INSERT INTO Subjects (Subject_Title, Subject_Weigt, Subject_Duration) VALUES (?, ?, ?)";
-         String lastRow = "SELECT TOP 1 FROM Subjects ORDER BY DESC";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, subject.getName());
-         ps.setInt(1, subject.getWeight());
-         ps.setInt(2, subject.getDuration());
-         ps.executeUpdate();
-         ResultSet rs = ps.executeQuery(lastRow);
-         return new Subject(rs.getInt(0),rs.getString(1), rs.getInt(2),rs.getInt(3));
-         
-     }
-     public Course insertCourse(Course course) throws SQLException{
-         String sql = "INSERT INTO Courses (Course_Title, Course_Weigt, Course_Duration) VALUES (?, ?, ?)";
-         String lastRow = "SELECT TOP 1 FROM Courses ORDER BY DESC";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, course.getTitle());
-         ps.setInt(1, course.getWeight());
-         ps.setInt(2, course.getDuration());
-         ps.executeUpdate();
-         ResultSet rs = ps.executeQuery(lastRow);
-         return new Course(rs.getInt(0),rs.getString(1), rs.getInt(2),rs.getInt(3));
-         
-     }
-     public boolean updateStudent(Student student) throws SQLException{
-         String sql = "UPDATE Students SET Student_Name = ?, Student_Surname = ?, Student_Age = ?, Student_Gender = ?, Student_Phone = ?, Student_Email = ?";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, student.getName());
-         ps.setString(1, student.getSurname());
-         ps.setString(2, student.getBirthDate());
-         ps.setString(3, student.getGender());
-         ps.setString(4, student.getPhone());
-         ps.setString(5, student.getEmail());
-         return ps.executeUpdate() > 0;
-         
-     }
-     public boolean updateLecturer(Lecturer lecturer) throws SQLException{
-         String sql = "UPDATE Lecturers SET Lecturer_Name = ?, Lecturer_Surname = ?, Lecturer_Age = ?, Lecturer_Gender = ?, Lecturer_Phone = ?, Lecturer_Email = ?";
-         PreparedStatement ps = Connect().prepareStatement(sql);
-         ps.setString(0, lecturer.getName());
-         ps.setString(1, lecturer.getSurname());
-         ps.setString(2, lecturer.getBirthDate());
-         ps.setString(3, lecturer.getGender());
-         ps.setString(4, lecturer.getPhone());
-         ps.setString(5, lecturer.getEmail());
-         return ps.executeUpdate() > 0;
-         
-     }
+
+    public List<Course> GetCourses() throws SQLException {
+        List<Course> courseList = new ArrayList<>();
+        Statement st = Connect().createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM Courses");
+        while (rs.next()) {
+            courseList.add(new Course(rs.getInt(0),
+                    rs.getString(1),
+                    rs.getInt(2),
+                    rs.getInt(3)));
+        }
+        return courseList;
+    }
+
+    public Student insertStudent(Student student) throws SQLException {
+        String sql = "INSERT INTO Students (Student_Name, Student_Surname, Student_Age, Student_Gender, Student_Phone, Student_Email) VALUES (?, ?, ?, ?, ?, ?)";
+        String lastRow = "SELECT TOP 1 FROM Students ORDER BY DESC";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, student.getName());
+        ps.setString(1, student.getSurname());
+        ps.setString(2, student.getBirthDate());
+        ps.setString(3, student.getGender());
+        ps.setString(4, student.getPhone());
+        ps.setString(5, student.getEmail());
+        ps.executeUpdate();
+        ResultSet rs = ps.executeQuery(lastRow);
+        return new Student(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+
+    }
+
+    public Lecturer insertLecturer(Lecturer lecturer) throws SQLException {
+        String sql = "INSERT INTO Lecturers (Lecturer_Name, Lecturer_Surname, Lecturer_Age, Lecturer_Gender, Lecturer_Phone, Lecturer_Email) VALUES (?, ?, ?, ?, ?, ?)";
+        String lastRow = "SELECT TOP 1 FROM Lecturers ORDER BY DESC";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, lecturer.getName());
+        ps.setString(1, lecturer.getSurname());
+        ps.setString(2, lecturer.getBirthDate());
+        ps.setString(3, lecturer.getGender());
+        ps.setString(4, lecturer.getPhone());
+        ps.setString(5, lecturer.getEmail());
+        ps.executeUpdate();
+        ResultSet rs = ps.executeQuery(lastRow);
+        return new Lecturer(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+
+    }
+
+    public Subject insertSubject(Subject subject) throws SQLException {
+        String sql = "INSERT INTO Subjects (Subject_Title, Subject_Weigt, Subject_Duration) VALUES (?, ?, ?)";
+        String lastRow = "SELECT TOP 1 FROM Subjects ORDER BY DESC";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, subject.getName());
+        ps.setInt(1, subject.getWeight());
+        ps.setInt(2, subject.getDuration());
+        ps.executeUpdate();
+        ResultSet rs = ps.executeQuery(lastRow);
+        return new Subject(rs.getInt(0), rs.getString(1), rs.getInt(2), rs.getInt(3));
+
+    }
+
+    public Course insertCourse(Course course) throws SQLException {
+        String sql = "INSERT INTO Courses (Course_Title, Course_Weigt, Course_Duration) VALUES (?, ?, ?)";
+        String lastRow = "SELECT TOP 1 FROM Courses ORDER BY DESC";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, course.getTitle());
+        ps.setInt(1, course.getWeight());
+        ps.setInt(2, course.getDuration());
+        ps.executeUpdate();
+        ResultSet rs = ps.executeQuery(lastRow);
+        return new Course(rs.getInt(0), rs.getString(1), rs.getInt(2), rs.getInt(3));
+
+    }
+
+    public boolean updateStudent(Student student) throws SQLException {
+        String sql = "UPDATE Students SET Student_Name = ?, Student_Surname = ?, Student_Age = ?, Student_Gender = ?, Student_Phone = ?, Student_Email = ?";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, student.getName());
+        ps.setString(1, student.getSurname());
+        ps.setString(2, student.getBirthDate());
+        ps.setString(3, student.getGender());
+        ps.setString(4, student.getPhone());
+        ps.setString(5, student.getEmail());
+        return ps.executeUpdate() > 0;
+
+    }
+
+    public boolean updateLecturer(Lecturer lecturer) throws SQLException {
+        String sql = "UPDATE Lecturers SET Lecturer_Name = ?, Lecturer_Surname = ?, Lecturer_Age = ?, Lecturer_Gender = ?, Lecturer_Phone = ?, Lecturer_Email = ?";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, lecturer.getName());
+        ps.setString(1, lecturer.getSurname());
+        ps.setString(2, lecturer.getBirthDate());
+        ps.setString(3, lecturer.getGender());
+        ps.setString(4, lecturer.getPhone());
+        ps.setString(5, lecturer.getEmail());
+        return ps.executeUpdate() > 0;
+
+    }
+    public boolean updateSubject(Subject subject) throws SQLException {
+        String sql = "UPDATE Subjects SET Subject_Title = ?, Subject_Weigt = ?, Subject_Duration = ?";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, subject.getName());
+        ps.setInt(1, subject.getWeight());
+        ps.setInt(2, subject.getDuration());
+        return ps.executeUpdate() > 0;
+    }
+    public boolean updateCourse(Course course) throws SQLException {
+        String sql = "UPDATE Courses SET Course_Title = ?, Course_Weigt = ?, Course_Duration = ?";
+        PreparedStatement ps = Connect().prepareStatement(sql);
+        ps.setString(0, course.getTitle());
+        ps.setInt(1, course.getWeight());
+        ps.setInt(2, course.getDuration());
+        return ps.executeUpdate() > 0;        
+    }
 }
